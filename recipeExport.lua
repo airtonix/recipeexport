f1d83c569c4dd9cbb154aced5cf3b6420c47a3c8
Zenobius Jiricek <airtonix@gmail.com>
2009-04-07 17:30:26 +0930

    include trigger button on trade window. made export window code more robust

--- recipeExport.lua
+++ recipeExport.lua
@@ -1,10 +1,11 @@
 recipeExport = CreateFrame"Frame"
+recipeExport:RegisterEvent("TRADE_SKILL_SHOW")
 recipeExport.siteUrl = 'http://www.wowhead.com'
 recipeExport.recipieUrl = recipeExport.siteUrl..'?spell='
 recipeExport.template = {
  bbcode = {
    book			= "[b][size=12pt]%s[/size][/b][i]%s/%s[/i]\n",
-   header		= "[b]%s[/b]\n",
+   header		= "\n[b]%s[/b]\n",
    item			= "[url=%s%s]%s[/url]\n",
    color			= "[color=%s]%s[/color]",
  },
@@ -21,6 +22,33 @@ recipeExport.template = {
    color			= "%s %s",
  },
 }
+
+function exportTradeList()
+ local txt = recipeExport:renderTradeList()
+ if not txt then print("No text provided, exiting."); return end
+ local window = recipeExport.window
+		   window.editor:SetText(txt)
+		   window:Show();
+		   window.editor:HighlightText()
+end
+
+recipeExport:SetScript("OnEvent", function() 
+	local event = event:lower()
+	if(recipeExport[event])then recipeExport[event]() end
+end)
+
+function recipeExport.trade_skill_show()
+	--Export Button
+	if(not _G['recipeExportViewRunButton'])then 
+	exportButton = CreateFrame("Button", "recipeExportViewRunButton", TradeSkillFrame, "OptionsButtonTemplate");
+	exportButton:SetText('[export]');
+	exportButton:SetWidth("48");
+	exportButton:SetNormalTexture(nil)
+	exportButton:SetHeight("20");
+	exportButton:SetPoint("TOPLEFT", TradeSkillLinkButton, "TOPRIGHT", 2, 1);
+	exportButton:SetScript("OnClick", exportTradeList);
+	end
+end
 
 function recipeExport:renderTradeList()
  local tradeSkillsNum, name, type
@@ -44,59 +72,47 @@ function recipeExport:renderTradeList()
  return output
 end
 
-function recipeExport:createTextArea(txt)
- if not txt then print("No text provided exiting."); return end
- if self.report == nil then
-  self.report=CreateFrame("Frame", "recipeExportView", UIParent);
- end
- 
- local report = self.report
-  report:Hide(); 
-  report:SetPoint("CENTER", "UIParent", "CENTER");
-  report:SetFrameStrata("DIALOG");
-  report:SetHeight(600);
-  report:SetWidth(800);
-  report:SetBackdrop({bgFile = "Interface/Tooltips/UI-Tooltip-Background",
+function recipeExport:CreateOutputWindow()
+  local window = CreateFrame("Frame", "recipeExportView", UIParent);
+  window:SetPoint("TOP", "UIParent", "TOP");
+  window:SetFrameStrata("DIALOG");
+  window:SetHeight(600);
+  window:SetWidth(800);
+  window:SetBackdrop({bgFile = "Interface/Tooltips/UI-Tooltip-Background",
     edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
     tile = true, tileSize = 16, edgeSize = 16,
     insets = { left = 9, right = 9, top = 9, bottom = 9 }
- });
-  report:SetBackdropColor(0, 0, 0, 0.8);
+  });
+  window:SetBackdropColor(0, 0, 0, 0.8);
   
   --Close Button
-  report.DoneButton = CreateFrame("Button", "recipeExportViewCloseButton", report, "OptionsButtonTemplate");
-  report.DoneButton:SetText('close');
-  report.DoneButton:SetPoint("BOTTOMRIGHT", report, "BOTTOMRIGHT", -10, 10);
-  report.DoneButton:SetScript("OnClick", function(this)
-  	report:Hide();
-  end);
+  window.closeButton = CreateFrame("Button", "recipeExportViewCloseButton", window, "OptionsButtonTemplate");
+  window.closeButton:SetText('close');
+  window.closeButton:SetPoint("TOPRIGHT", window, "TOPRIGHT", -10, -6);
+  window.closeButton:SetScript("OnClick", function(this) window:Hide(); end);
   
   --ScrollBar
-  report.Scroll = CreateFrame("ScrollFrame", "recipeExportViewScrollFrame", report, "UIPanelScrollFrameTemplate");
-  report.Scroll:SetPoint("TOPLEFT", report, "TOPLEFT", 20, -20);
-  report.Scroll:SetPoint("RIGHT", report, "RIGHT", -30, 0);
-  report.Scroll:SetPoint("BOTTOM", report.DoneButton, "TOP", 0, 20);
+  window.scrollbar = CreateFrame("ScrollFrame", "recipeExportViewScrollBar", window, "UIPanelScrollFrameTemplate");
+  window.scrollbar:SetPoint("TOPLEFT", window, "TOPLEFT", 20, -20);
+  window.scrollbar:SetPoint("RIGHT", window, "RIGHT", -30, 0);
+  window.scrollbar:SetPoint("BOTTOM", window, "BOTTOM", 0, 20);
   
   --TextArea
-  report.Box = CreateFrame("EditBox", "recipeExportViewEditBox", report.Scroll);
-  report.Box:SetWidth(750);
-  report.Box:SetHeight(85);
-  report.Box:SetFontObject("ChatFontNormal");
-  report.Box:SetMultiLine(true);
-  report.Scroll:SetScrollChild(report.Box);
-  report.Box:SetText(txt);
-  report.Box:HighlightText();  
-  report.Box:SetScript("OnTextChanged", function(this)
-    report.Scroll:UpdateScrollChildRect();
-  end);
-  report:Show();
-  report.Box:HighlightText();  
+  window.editor = CreateFrame("EditBox", "recipeExportViewEditBox",window.scrollbar);
+  window.editor:SetFontObject("ChatFontNormal");
+  window.editor:SetWidth(750);
+  window.editor:SetHeight(85);
+  window.editor:SetMultiLine(true);
+  window.scrollbar:SetScrollChild(window.editor);
+  window.editor:SetScript("OnTextChanged", function(this) window.scrollbar:UpdateScrollChildRect(); end);
+  window:Hide();
+  self.window = window;
+  
 end
+recipeExport:CreateOutputWindow()
+
 
 SLASH_TRADESKILL1 = "/tradex";
 SLASH_TRADESKILL2 = "/rex";
-SlashCmdList["TRADESKILL"] = function()
- local txt = recipeExport:renderTradeList()
- if(txt)then recipeExport:createTextArea(txt) end
-end;
+SlashCmdList["TRADESKILL"] = exportTradeList
 
